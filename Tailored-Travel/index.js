375350b3ae210edafc2e1c9f5b540f6063503c9d
Joey <joeyhoellerich@gmail.com>
2022-04-27 18:21:38 -0400

    added psuedocode

---
+++ Tailored-Travel/index.js
@@ -0,0 +1,16 @@
+// Obtain user location
+// use leaflet's get coordinate built in function
+// return user's latitude and longitude
+
+// Center Map based on user's latitude and longitude
+// zoom in on map (set minzoom value)
+// add marker on user's position
+
+
+// grab submit button from DOM
+
+// create function for submit button
+// get value from dropdown menu (resturant, business, coffee, etc)
+// use value to identify 5 closest places based on user Lat/Long - Foursquare API
+// create obejct for each location with location name, and Lat/Long Values
+// place locations on map using markers (add location names to markers) - Leaflet API
