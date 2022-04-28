62124f012b1597d48bd15ef9ab2278814fcf5551
Joey <joeyhoellerich@gmail.com>
2022-04-27 20:29:52 -0400

    added foursquare js

--- Tailored-Travel/foursquare.js
+++ Tailored-Travel/foursquare.js
@@ -1,12 +1,40 @@
-const options = {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      Authorization: 'fsq3l/7/KnqGnbRmkhzeDFBm4FLyYBdHTegUn9nzRUJqvZY='
-    }
-  };
-  
-  fetch('https://api.foursquare.com/v3/places/search', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+// Fetch Foursquare results
+
+// categories codes
+// coffee = 13032
+// resaurant = 13065
+// business = 11000
+// hotel = 19014
+
+
+// search function
+// /search?ll=###%2C###&categories=#####%2C#####%2C######&limit=5
+// ll=LAT%2CLONG - sets starting location based on LAT and LONG
+// categories=#####%2C##### - sets search criteria categories based on codes 
+// limit=# - sets number of results
+
+
+let array = []
+// create async function that get list of businesses based on user's location, and category selected
+async function getBusinessesList(lat, long, businessCode){
+
+    // set options for fetching information from foursquare (from API)
+    const options = {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'fsq3l/7/KnqGnbRmkhzeDFBm4FLyYBdHTegUn9nzRUJqvZY='
+        }
+      };
+
+    let searchQuery = `https://api.foursquare.com/v3/places/search?ll=${lat}%2C${long}&categories=${businessCode}&limit=5`
+      
+    // fetch information from foursquare places, store in array called businessList   
+    let businessList = await fetch(searchQuery, options)
+        .then(response => response.json())
+        .catch(err => console.error(err));
+
+    return businessList
+}
+
+getBusinessesList(57.8, -57.8, 13032).then(results => console.log(results))
--- Tailored-Travel/index.js
+++ Tailored-Travel/index.js
@@ -8,7 +8,7 @@ async function getCoords(){
     // check and see if the user has geolocation
     if (navigator.geolocation){
         // create a new promise object that will be resolved with the user's geolocation data
-        new Promise((resolve) => {
+        return new Promise((resolve) => {
             // gets the current position information from user, resolves promise when completed
             navigator.geolocation.getCurrentPosition(resolve)
         }).then((value) => {
@@ -22,9 +22,43 @@ async function getCoords(){
     }  
 }
 
-getCoords();
-// use leaflet's get coordinate built in function - async
-// return user's latitude and longitude
+// create Map
+async function makeMap(){
+
+    let mapObj = document.getElementById("map");
+
+    // get user's coords and set them to user.latitude and user.longitude
+    await getCoords();
+
+    // check to make sure that map coordinates are available
+    if (user.latitude == null || user.longitude == null){
+        // if no map coordinates are available display message below
+        mapObj.innerHTML = "Sorry No Map!"
+        return
+    }
+
+    // create new map object on the div with the map id
+    let map = L.map("map", {
+        // center map on the user's latitude and longitude
+        center: [user.latitude, user.longitude],
+        zoom: 15
+    })
+
+    // create tile layer for the map (copy usage from Leaflet)
+    // add to the map variable
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', 
+        {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+
+    // add maker to represent the user
+    // place marker at the user's latitude and longitude
+    let userMarker = L.marker([user.latitude, user.longitude]);
+    // bind the marker with a popup stating: you are here!
+    userMarker.bindPopup(`<pl><b>You are Here!<br>${user.latitude}, ${user.longitude}</b></pl>`);
+    // add the markert to the map, and open it by default
+    userMarker.addTo(map).openPopup();
+}
+
+makeMap()
 
 // Center Map based on user's latitude and longitude
 // zoom in on map (set minzoom value)
